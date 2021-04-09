c66581b87c7721443d5ef0c0dc11c81589888822
OMID <ahmadpooromid@gmail.com>
2021-04-09 19:28:52 +0430

    register just redis cache for first release.

--- PolyCache/DependencyInjection.cs
+++ PolyCache/DependencyInjection.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using PolyCache.Cache;
 using PolyCache.Configuration;
-using PolyCache.Infrastructure;
 
 namespace PolyCache
 {
@@ -16,57 +15,39 @@ namespace PolyCache
             var appSettings = new AppSettings();
             configuration.Bind(appSettings);
             services.AddSingleton(appSettings);
-            //AppSettingsHelper.SaveAppSettings(appSettings);
 
-            //nop cache config
-            //var appSettings = Singleton<AppSettings>.Instance;
             var distributedCacheConfig = appSettings.DistributedCacheConfig;
 
             if (!distributedCacheConfig.Enabled)
                 return services;
 
-            switch (distributedCacheConfig.DistributedCacheType)
+            services.AddStackExchangeRedisCache(options =>
             {
-                case DistributedCacheType.Memory:
-                    services.AddDistributedMemoryCache();
-                    break;
+                options.Configuration = distributedCacheConfig.ConnectionString;
+            });
 
-                case DistributedCacheType.SqlServer:
-                    services.AddDistributedSqlServerCache(options =>
-                    {
-                        options.ConnectionString = distributedCacheConfig.ConnectionString;
-                        options.SchemaName = distributedCacheConfig.SchemaName;
-                        options.TableName = distributedCacheConfig.TableName;
-                    });
-                    break;
-
-                case DistributedCacheType.Redis:
-                    services.AddStackExchangeRedisCache(options =>
-                    {
-                        options.Configuration = distributedCacheConfig.ConnectionString;
-                    });
-                    break;
-            }
-
-            //var appSettings = Singleton<AppSettings>.Instance;
-
-            //services.AddStackExchangeRedisCache(options =>
+            //switch (distributedCacheConfig.DistributedCacheType)
             //{
-            //    options.Configuration = "localhost:6379";
-            //});
+            //    case DistributedCacheType.Memory:
+            //        services.AddDistributedMemoryCache();
+            //        break;
 
-            //services.AddScoped<IStaticCacheManager, DistributedCacheManager>();
+            //    case DistributedCacheType.SqlServer:
+            //        services.AddDistributedSqlServerCache(options =>
+            //        {
+            //            options.ConnectionString = distributedCacheConfig.ConnectionString;
+            //            options.SchemaName = distributedCacheConfig.SchemaName;
+            //            options.TableName = distributedCacheConfig.TableName;
+            //        });
+            //        break;
 
-            ////add configuration parameters
-            //var appSetting = new AppSettings();
-            //services.AddSingleton(appSetting);
-
-            //var appOptions = configuration.GetSection(nameof(AppOptions)).Get<AppOptions>();
-
-            //services.AddStackExchangeRedisCache(options =>
-            //{
-            //    options.Configuration = appOptions.RedisConnectionString;
-            //});
+            //    case DistributedCacheType.Redis:
+            //        services.AddStackExchangeRedisCache(options =>
+            //        {
+            //            options.Configuration = distributedCacheConfig.ConnectionString;
+            //        });
+            //        break;
+            //}
 
             return services;
         }
