167f348025165f1c535e5112eb0c1506ef52398c
OMID <ahmadpooromid@gmail.com>
2021-04-09 17:00:00 +0430

    change cache registeration based on appsettings configuration.

--- PolyCache/DependencyInjection.cs
+++ PolyCache/DependencyInjection.cs
@@ -10,20 +10,56 @@ namespace PolyCache
     {
         public static IServiceCollection AddDistributedCache(this IServiceCollection services, IConfiguration configuration)
         {
-            var appSettings = Singleton<AppSettings>.Instance;
-            //var distributedCacheConfig = appSettings.DistributedCacheConfig;
-
-            services.AddStackExchangeRedisCache(options =>
-            {
-                options.Configuration = "localhost:6379";
-            });
-
             services.AddScoped<IStaticCacheManager, DistributedCacheManager>();
 
             //add configuration parameters
-            var appSetting = new AppSettings();
-            //configuration.Bind(appSetting);
-            services.AddSingleton(appSetting);
+            var appSettings = new AppSettings();
+            configuration.Bind(appSettings);
+            services.AddSingleton(appSettings);
+            //AppSettingsHelper.SaveAppSettings(appSettings);
+
+            //nop cache config
+            //var appSettings = Singleton<AppSettings>.Instance;
+            var distributedCacheConfig = appSettings.DistributedCacheConfig;
+
+            if (!distributedCacheConfig.Enabled)
+                return services;
+
+            switch (distributedCacheConfig.DistributedCacheType)
+            {
+                case DistributedCacheType.Memory:
+                    services.AddDistributedMemoryCache();
+                    break;
+
+                case DistributedCacheType.SqlServer:
+                    services.AddDistributedSqlServerCache(options =>
+                    {
+                        options.ConnectionString = distributedCacheConfig.ConnectionString;
+                        options.SchemaName = distributedCacheConfig.SchemaName;
+                        options.TableName = distributedCacheConfig.TableName;
+                    });
+                    break;
+
+                case DistributedCacheType.Redis:
+                    services.AddStackExchangeRedisCache(options =>
+                    {
+                        options.Configuration = distributedCacheConfig.ConnectionString;
+                    });
+                    break;
+            }
+
+            //var appSettings = Singleton<AppSettings>.Instance;
+
+            //services.AddStackExchangeRedisCache(options =>
+            //{
+            //    options.Configuration = "localhost:6379";
+            //});
+
+            //services.AddScoped<IStaticCacheManager, DistributedCacheManager>();
+
+            ////add configuration parameters
+            //var appSetting = new AppSettings();
+            //services.AddSingleton(appSetting);
 
             //var appOptions = configuration.GetSection(nameof(AppOptions)).Get<AppOptions>();
 
