63314899a15883d3971ee6d4c164033dbbeceddd
omid ahmadpour <ahmadpooromid@gmail.com>
2023-09-22 20:57:23 +0200

    some minor refactoring.

--- Sample/Services/WeatherService.cs
+++ Sample/Services/WeatherService.cs
@@ -27,20 +27,20 @@ namespace Sample.Services
             var result = await staticCacheManager.GetWithExpireTimeAsync(
                 new CacheKey(CachePrefix + cityName),
                 CacheExpiryTime,
-                async () => await GetWeather());
+                GetWeather);
 
             return result;
 
-            async Task<IEnumerable<WeatherForecast>> GetWeather()
+            Task<IEnumerable<WeatherForecast>> GetWeather()
             {
                 var rng = new Random();
-                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                return Task.FromResult<IEnumerable<WeatherForecast>>(Enumerable.Range(1, 5).Select(index => new WeatherForecast
                 {
                     Date = DateTime.Now.AddDays(index),
                     TemperatureC = rng.Next(-20, 55),
                     Summary = Summaries[rng.Next(Summaries.Length)]
                 })
-                .ToArray();
+                .ToArray());
             }
         }
     }
